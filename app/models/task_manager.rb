886b53680bb9f481cd3607ca9d2755e8d9c61773
July Tran <julyytran@gmail.com>
2016-01-26 11:00:21 -0700

    changed class methods to instance methods

--- app/models/task_manager.rb
+++ app/models/task_manager.rb
@@ -2,11 +2,13 @@ require 'yaml/store'
 require_relative 'task'
 
 class TaskManager
-  def self.database
-    @database ||= YAML::Store.new("db/task_manager")
+  attr_reader :database
+
+  def initialize(database)
+    @database = database
   end
 
-  def self.create(task)
+  def create(task)
     database.transaction do
       database['tasks'] ||= []
       database['total'] ||= 0
@@ -15,21 +17,21 @@ class TaskManager
     end
   end
 
-  def self.raw_tasks
+  def raw_tasks
     database.transaction do
       database['tasks'] || []
     end
   end
 
-  def self.all
+  def all
     raw_tasks.map { |data| Task.new(data) }
   end
 
-  def self.raw_task(id)
+  def raw_task(id)
     raw_tasks.find { |task| task["id"] == id }
   end
 
-  def self.find(id)
+  def find(id)
     Task.new(raw_task(id))
   end
 end
